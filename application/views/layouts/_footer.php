a266fcccf592d0a971464a55734d1fee02e2edb5
Muhamad Ilham <muhamadilhamm48@gmail.com>
2022-06-20 20:36:52 +0700

    first commit

---
+++ application/views/layouts/_footer.php
@@ -0,0 +1,8 @@
+<footer class="footer mt-5">
+	<nav class="navbar navbar-dark bg-light sticky-bottom">
+		<div class="container d-flex justify-content-center">
+			<div class="copyright text-info p-3">
+</div>
+		</div>
+	</nav>	
+</footer>  
